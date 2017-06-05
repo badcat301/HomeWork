dc0c20b8e360000b841d7a798900000846c1fe1c
badcat301 <badcat301@gmail.com>
2017-06-05 12:30:11 -0400

    commit

--- lessons14/src/AppFX/controller/Autorize.java
+++ lessons14/src/AppFX/controller/Autorize.java
@@ -1,7 +1,27 @@
 package AppFX.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 /**
  * Created by adm on 05.06.2017.
  */
 public class Autorize {
+    @FXML
+    TextField emailLogInTextF;
+
+    @FXML
+    PasswordField passLogIPassF;
+
+    public void sInButton(ActionEvent actionEvent) {
+
+
+    }
+
+    public void lInButton(ActionEvent actionEvent) {
+
+
+    }
 }
--- lessons14/src/AppFX/controller/Registration.java
+++ lessons14/src/AppFX/controller/Registration.java
@@ -4,15 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -34,6 +30,48 @@ public class Registration implements Initializable {
 
     @FXML
     public void buttonReg(ActionEvent actionEvent) throws SQLException, IOException {
+        addNewUserAndCheck();
+    }
+
+    @FXML
+    public void logInButton1(ActionEvent actionEvent) throws IOException {
+        secondStageLoad();
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        sqliteWork();
+    }
+
+    private void sqliteWork() {
+        dbConnect.dBCheck();
+        dbConnect.getDBConnection();
+
+        try {
+            dbConnect.createDbUserTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private void showDialog() {
+        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+        dialog.initStyle(StageStyle.UTILITY);
+        dialog.setTitle("Инфо");
+        dialog.setHeaderText("Пользователь существует." + "\n" + "Введите другой Email");
+        dialog.showAndWait();
+    }
+    private void secondStageLoad() throws IOException {
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/autorize.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.setTitle("LogIN");
+        stage.show();
+
+
+    }
+    private void addNewUserAndCheck() throws SQLException, IOException {
         final boolean found = dbConnect.findByDbTable(email.getText());
         if (found) {
             showDialog();
@@ -46,46 +84,5 @@ public class Registration implements Initializable {
             secondStageLoad();
         }
     }
-
-    @FXML
-    public void logInButton(ActionEvent actionEvent) throws IOException {
-        secondStageLoad();
-    }
-
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        dbConnect.dBCheck();
-        dbConnect.getDBConnection();
-
-        try {
-            dbConnect.createDbUserTable();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private void showDialog() {
-        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
-        dialog.initStyle(StageStyle.UTILITY);
-        dialog.setTitle("Инфо");
-        dialog.setHeaderText("Пользователь существует." + "\n" + "Введите другой Email");
-        dialog.showAndWait();
-    }
-
-    // этот метод неработает
-    private void secondStageLoad() throws IOException {
-
-
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("autorize.fxml"));
-        Parent root1 = (Parent) fxmlLoader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root1));
-        stage.show();
-
-
-    }
-
 }
 
